fd848b59ef4e90ab430a9cc9862d3b59046175c1
KristieCunha <kristie.cunha@gmail.com>
2025-07-09 11:56:07 -0400

    customer Id working

--- src/actions/checkout.js
+++ src/actions/checkout.js
@@ -6,7 +6,9 @@ import axios, { fetcher, endpoints } from 'src/lib/axios';
 const CHECKOUT_ENDPOINT = endpoints.checkout;
 
 export async function createCheckoutSession(priceId, userId, quantity) {
-    const customerId = await getCustomer(userId)
+    const customer = await getCustomer(userId)
+    const customerId = customer.StripeID;
+    console.log(customerId)
     const data = { priceId, customerId, quantity };
     const response = await axios.post(CHECKOUT_ENDPOINT, data, { params: { endpoint: 'create-checkout-session' } });
     window.location.replace(response.data.url);
--- src/actions/customer.js
+++ src/actions/customer.js
@@ -7,6 +7,7 @@ const CUSTOMER_ENDPOINT = endpoints.customer;
 export async function getCustomer(id) {
     const data = { id };
     const response = await axios.post(CUSTOMER_ENDPOINT, data, { params: { endpoint: 'get-customer' } })
-    const customer_id = response.data.customer
-    return customer_id
+    const customer = await response.data
+
+    return customer
 }
