a893325749fe7bc593ef7792e14f7975bb2d7d5e
KristieCunha <kristie@houseofluaconsulting.com>
2025-08-29 18:53:28 -0600

    Update checkout.js

--- src/actions/checkout.js
+++ src/actions/checkout.js
@@ -8,7 +8,6 @@ const CHECKOUT_ENDPOINT = endpoints.checkout;
 export async function createCheckoutSession(priceId, userId, quantity) {
     const customer = await getCustomer(userId)
     const stripeId = customer.StripeId;
-    console.log(stripeId)
     const data = { priceId, stripeId, quantity };
     const response = await axios.post(CHECKOUT_ENDPOINT, data, { params: { endpoint: 'create-checkout-session' } });
     window.location.replace(response.data.url);
