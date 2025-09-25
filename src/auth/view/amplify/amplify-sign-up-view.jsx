3320fae8f91f8eab70c21f1708f8ecd30cedfe93
KristieCunha <kristie@houseofluaconsulting.com>
2025-09-25 09:38:02 -0600

    Update amplify-sign-up-view.jsx

--- src/auth/view/amplify/amplify-sign-up-view.jsx
+++ src/auth/view/amplify/amplify-sign-up-view.jsx
@@ -149,7 +149,7 @@ export function AmplifySignUpView() {
   return (
     <>
       <FormHead
-        title="Create an Account"
+        title="Create an account"
         description={
           <>
             {`Already have an account? `}
