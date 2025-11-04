f96ce2eb317d6bf7cdbc7f71db55aa10db741345
Kristie <kristie@houseofluaconsulting.com>
2025-11-04 07:59:18 -0700

    Update _others.js

--- src/_mock/_others.js
+++ src/_mock/_others.js
@@ -152,7 +152,7 @@ export const _veteranWebsitePricingPlans = [
     amount: '$1170.00',
     lists: ['Disconnected number replacement', 'Exclusive Leads', 'Live lead transfer directly to your CRM', 'Includes call scripts, text templates, and weekly team Q&A sessions'],
     labelAction: 'Purchase',
-    priceId: 'price_1SPYKuGHAeFkU5hvg2mQqJGG',
+    priceId: 'price_1SPlfYGHAeFkU5hvDU9jdYN3',
   },
   {
     credit: '40',
