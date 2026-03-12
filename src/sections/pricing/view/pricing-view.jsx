a936505fca8cce58b51e2ce08f26b96e8ac3fa0e
Kristie <kristie@houseofluaconsulting.com>
2026-03-12 17:20:24 -0400

    Update pricing-view.jsx

--- src/sections/pricing/view/pricing-view.jsx
+++ src/sections/pricing/view/pricing-view.jsx
@@ -131,7 +131,7 @@ export function PricingView() {
               severity="info"
               sx={{ mb: 2, display: 'flex', alignItems: 'center' }}
             >
-              Purchasing of <b>Legacy Leads</b> is suspended to fill current orders. Purchasing will resume as soon as possible. Thank you for your patience!
+              Purchasing of Fresh <b>Legacy Leads</b> is suspended to fill current orders. Purchasing will resume as soon as possible. Thank you for your patience!
             </Alert>
           </Box>
 
@@ -161,7 +161,7 @@ export function PricingView() {
               <MixedPricingCard key={card.priceId} card={card} index={index} />
             ))}
           </Box> */}
-          {/* <Box
+          <Box
             sx={{
               display: 'grid',
               gap: { xs: 3, md: 0 },
@@ -173,7 +173,7 @@ export function PricingView() {
             {_legacyWebsiteAgedPricingPlans.map((card, index) => (
               <PricingCard key={card.priceId} card={card} index={index} />
             ))}
-          </Box> */}
+          </Box>
         </>
       ) : (
         <Alert
