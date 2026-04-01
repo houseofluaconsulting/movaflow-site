d03a3bdfd6478bcaf60ef0c0d55b6ca0ad423aae
Kristie <kristie@houseofluaconsulting.com>
2026-04-01 08:11:12 -0600

    Revert "Update pricing-view.jsx"
    This reverts commit 3e68234b02980f0274deb82acc600dd7e83225d7.

--- src/sections/pricing/view/pricing-view.jsx
+++ src/sections/pricing/view/pricing-view.jsx
@@ -90,19 +90,11 @@ export function PricingView() {
         Purchase Leads
       </Typography>
 
-      <Alert
-          variant="outlined"
-          severity="info"
-          sx={{ mt: 3, display: 'flex', alignItems: 'center' }}
-        >
-          We are currently experiecing issues with Stripe. Purchasing of leads has been temporarily disabled while we resolve the issue. Thank you for your patience and understanding.
-        </Alert>
-
       
 
       {showPricing ? (
         <>
-          {/* {availableTabs.length > 0 && (
+          {availableTabs.length > 0 && (
             <Tabs
               value={currentTab}
               onChange={(_, newValue) => setCurrentTab(newValue)}
@@ -113,11 +105,11 @@ export function PricingView() {
                 <Tab key={tab.key} label={tab.label} />
               ))}
             </Tabs>
-          )} */}
+          )}
 
           {/* VeteranWebsite */}
 
-          {/* {availableTabs[currentTab]?.key === 'VeteranWebsite' && (
+          {availableTabs[currentTab]?.key === 'VeteranWebsite' && (
             <>
               <Box
                 sx={{
@@ -147,10 +139,10 @@ export function PricingView() {
                 ))}
               </Box>
             </>
-          )} */}
+          )}
 
           {/* LegacyWebsite */}
-          {/* {availableTabs[currentTab]?.key === 'LegacyWebsite' && (
+          {availableTabs[currentTab]?.key === 'LegacyWebsite' && (
             <>
               <Box
                 sx={{
@@ -194,7 +186,7 @@ export function PricingView() {
                 ))}
               </Box>
             </>
-          )} */}
+          )}
         </>
       ) : fetchError ? (
         <Alert
